d5ea8365f7ac1183c997915d0a210ab8d18597df
Jorrit Salverda <jsalverda@travix.com>
2017-08-09 16:50:32 +0200

    set default fields for all log messages

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"os"
 	"runtime"
 	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -48,31 +50,38 @@ func init() {
 
 func main() {
 
-	log.Info().
+	// set some default fields added to all logs
+	log := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Str("app", "estafette-ci-api").
 		Str("version", version).
+		Logger()
+
+	// log startup message
+	log.Info().
 		Str("branch", branch).
 		Str("revision", revision).
 		Str("buildDate", buildDate).
 		Str("goVersion", goVersion).
-		Msg("Starting estafette-ci-api")
+		Msg("Starting estafette-ci-api...")
 
 	// start prometheus
 	go func() {
 		log.Info().
 			Str("port", *prometheusAddress).
 			Str("path", *prometheusMetricsPath).
-			Msg("Serving Prometheus metrics")
+			Msg("Serving Prometheus metrics...")
 
 		http.Handle(*prometheusMetricsPath, promhttp.Handler())
 
 		if err := http.ListenAndServe(*prometheusAddress, nil); err != nil {
-			log.Fatal().Err(err).Msg("Prometheus listener failed")
+			log.Fatal().Err(err).Msg("Starting Prometheus listener failed")
 		}
 	}()
 
 	log.Info().
 		Str("port", *apiAddress).
-		Msg("Serving api calls")
+		Msg("Serving api calls...")
 
 	http.HandleFunc("/webhook/github", githubWebhookHandler)
 	http.HandleFunc("/webhook/bitbucket", bitbucketWebhookHandler)
@@ -80,7 +89,7 @@ func main() {
 	http.HandleFunc("/readiness", readinessHandler)
 
 	if err := http.ListenAndServe(*apiAddress, nil); err != nil {
-		log.Fatal().Err(err).Msg("Api listener failed")
+		log.Fatal().Err(err).Msg("Starting api listener failed")
 	}
 }
 
