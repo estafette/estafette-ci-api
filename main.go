29005b0e3a62e7e88b1c9a3006fd27ddd7cddc55
Jorrit Salverda <jorrit.salverda@gmail.com>
2017-08-10 15:53:25 +0200

    deserialize bitbucket repo:push event

--- main.go
+++ main.go
@@ -282,7 +282,61 @@ func handleGithubPush(body []byte) {
 	log.Info().Interface("pushEvent", pushEvent).Msgf("Deserialized GitHub push event for repository %v", pushEvent.Repository.FullName)
 }
 
+// BitbucketRepositoryPushEvent represents a Bitbucket webhook push event
+type BitbucketRepositoryPushEvent struct {
+	Actor      BitbucketOwner      `json:"actor"`
+	Repository BitbucketRepository `json:"repository"`
+	Push       BitbucketPushEvent  `json:"push"`
+}
+
+// BitbucketPushEvent represents a Bitbucket push event push info
+type BitbucketPushEvent struct {
+	Changes []BitbucketPushEventChange `json:"changes"`
+}
+
+// BitbucketPushEventChange represents a Bitbucket push change
+type BitbucketPushEventChange struct {
+	New       BitbucketPushEventChangeObject `json:"new,omitempty"`
+	Old       BitbucketPushEventChangeObject `json:"old,omitempty"`
+	Created   bool                           `json:"created"`
+	Closed    bool                           `json:"closed"`
+	Forced    bool                           `json:"forced"`
+	Truncated bool                           `json:"truncated"`
+}
+
+// BitbucketPushEventChangeObject represents the state of the reference after a push
+type BitbucketPushEventChangeObject struct {
+	Type string `json:"type"`
+	Name string `json:"name,omitempty"`
+}
+
+// BitbucketOwner represents a Bitbucket owern
+type BitbucketOwner struct {
+	Type        string `json:"type"`
+	UserName    string `json:"username"`
+	DisplayName string `json:"display_name"`
+}
+
+// BitbucketRepository represents a Bitbucket repository
+type BitbucketRepository struct {
+	Name      string         `json:"name"`
+	FullName  string         `json:"full_name"`
+	Owner     BitbucketOwner `json:"owner"`
+	IsPrivate bool           `json:"is_private"`
+	Scm       string         `json:"scm"`
+}
+
 func handleBitbucketPush(body []byte) {
+
+	// unmarshal json body
+	var pushEvent BitbucketRepositoryPushEvent
+	err := json.Unmarshal(body, &pushEvent)
+	if err != nil {
+		log.Error().Err(err).Str("body", string(body)).Msg("Deserializing body to BitbucketRepositoryPushEvent failed")
+		return
+	}
+
+	log.Info().Interface("pushEvent", pushEvent).Msgf("Deserialized Bitbucket push event for repository %v", pushEvent.Repository.FullName)
 
 }
 
