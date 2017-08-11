77b0b1d69c5215aaa76b10b6dd45dcf604d830aa
Jorrit Salverda <jsalverda@travix.com>
2017-08-11 12:00:47 +0200

    vendor google/go-github; add and mount secret for github app private key

--- govendor.sh
+++ govendor.sh
@@ -3,3 +3,4 @@ govendor fetch github.com/ericchiang/k8s@v0.2.0
 govendor fetch github.com/prometheus/client_golang/prometheus@v0.8.0
 govendor fetch github.com/prometheus/client_golang/prometheus/promhttp@v0.8.0
 govendor fetch github.com/rs/zerolog/log@v1.2.2
+govendor fetch github.com/google/go-github/github # no tags or releases :S https://github.com/google/go-github/issues/689
