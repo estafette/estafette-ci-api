4e9743c1322c72b698a811d56369cac2b8ed83bf
Jorrit Salverda <jsalverda@travix.com>
2017-08-14 17:39:29 +0200

    for bitbucket create a job with estafette-ci-builder

--- kubernetes.go
+++ kubernetes.go
@@ -22,7 +22,6 @@ type Kubernetes struct {
 type KubernetesClient interface {
 	CreateJobForGithubPushEvent(GithubPushEvent, string) (*batchv1.Job, error)
 	CreateJobForBitbucketPushEvent(BitbucketRepositoryPushEvent, string) (*batchv1.Job, error)
-	createJob(string, []string) (*batchv1.Job, error)
 }
 
 // NewKubernetesClient return a Kubernetes client
@@ -55,28 +54,6 @@ func (k *Kubernetes) CreateJobForGithubPushEvent(pushEvent GithubPushEvent, auth
 
 	args := []string{"clone", "--depth=10", authenticatedGitURL}
 
-	return k.createJob(jobName, args)
-}
-
-// CreateJobForBitbucketPushEvent creates a kubernetes job to clone the authenticated git url
-func (k *Kubernetes) CreateJobForBitbucketPushEvent(pushEvent BitbucketRepositoryPushEvent, authenticatedGitURL string) (job *batchv1.Job, err error) {
-
-	re := regexp.MustCompile("[^a-zA-Z0-9]+")
-	repoName := re.ReplaceAllString(pushEvent.Repository.FullName, "-")
-	if len(repoName) > 50 {
-		repoName = repoName[:50]
-	}
-
-	// max 63 chars
-	jobName := strings.ToLower(fmt.Sprintf("build-%v-%v", repoName, pushEvent.Push.Changes[0].New.Target.Hash[:6]))
-
-	args := []string{"clone", "--depth=10", fmt.Sprintf("--branch=%v", pushEvent.Push.Changes[0].New.Name), authenticatedGitURL}
-
-	return k.createJob(jobName, args)
-}
-
-func (k *Kubernetes) createJob(jobName string, args []string) (job *batchv1.Job, err error) {
-
 	containerName := "git-clone"
 	image := "alpine/git"
 	restartPolicy := "Never"
@@ -102,6 +79,76 @@ func (k *Kubernetes) createJob(jobName string, args []string) (job *batchv1.Job,
 							Name:  &containerName,
 							Image: &image,
 							Args:  args,
+						},
+					},
+					RestartPolicy: &restartPolicy,
+				},
+			},
+		},
+	}
+
+	job, err = k.Client.BatchV1().CreateJob(context.Background(), job)
+
+	return
+}
+
+// CreateJobForBitbucketPushEvent creates a kubernetes job to clone the authenticated git url
+func (k *Kubernetes) CreateJobForBitbucketPushEvent(pushEvent BitbucketRepositoryPushEvent, authenticatedGitURL string) (job *batchv1.Job, err error) {
+
+	re := regexp.MustCompile("[^a-zA-Z0-9]+")
+	repoName := re.ReplaceAllString(pushEvent.Repository.FullName, "-")
+	if len(repoName) > 50 {
+		repoName = repoName[:50]
+	}
+
+	// max 63 chars
+	jobName := strings.ToLower(fmt.Sprintf("build-%v-%v", repoName, pushEvent.Push.Changes[0].New.Target.Hash[:6]))
+
+	estafetteGitURLName := "ESTAFETTE_GIT_URL"
+	estafetteGitURLValue := authenticatedGitURL
+	estafetteGitBranchName := "ESTAFETTE_GIT_BRANCH"
+	estafetteGitBranchValue := pushEvent.Push.Changes[0].New.Name
+	estafetteGitRevisionName := "ESTAFETTE_GIT_REVISION"
+	estafetteGitRevisionValue := pushEvent.Push.Changes[0].New.Target.Hash
+
+	containerName := "estafette-ci-builder"
+	image := "estafette/estafette-ci-builder:0.0.151"
+	restartPolicy := "Never"
+
+	job = &batchv1.Job{
+		Metadata: &metav1.ObjectMeta{
+			Name:      &jobName,
+			Namespace: &k.Client.Namespace,
+			Labels: map[string]string{
+				"createdBy": "estafette",
+			},
+		},
+		Spec: &batchv1.JobSpec{
+			Template: &apiv1.PodTemplateSpec{
+				Metadata: &metav1.ObjectMeta{
+					Labels: map[string]string{
+						"createdBy": "estafette",
+					},
+				},
+				Spec: &apiv1.PodSpec{
+					Containers: []*apiv1.Container{
+						&apiv1.Container{
+							Name:  &containerName,
+							Image: &image,
+							Env: []*apiv1.EnvVar{
+								&apiv1.EnvVar{
+									Name:  &estafetteGitURLName,
+									Value: &estafetteGitURLValue,
+								},
+								&apiv1.EnvVar{
+									Name:  &estafetteGitBranchName,
+									Value: &estafetteGitBranchValue,
+								},
+								&apiv1.EnvVar{
+									Name:  &estafetteGitRevisionName,
+									Value: &estafetteGitRevisionValue,
+								},
+							},
 						},
 					},
 					RestartPolicy: &restartPolicy,
