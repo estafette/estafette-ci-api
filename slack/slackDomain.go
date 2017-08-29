5c9282b7f9af70963467068bf9ce7825ed955a5e
Jorrit Salverda <jsalverda@travix.com>
2017-08-29 16:27:04 +0200

    add properties to slashcommand struct

--- slack/slackDomain.go
+++ slack/slackDomain.go
@@ -2,4 +2,16 @@ package slack
 
 // SlashCommand represents a slash command from Slack
 type SlashCommand struct {
+	Token          string `form:"token"`
+	TeamID         string `form:"team_id"`
+	TeamDomain     string `form:"team_domain"`
+	EnterpriseID   string `form:"enterprise_id"`
+	EnterpriseName string `form:"enterprise_name"`
+	ChannelID      string `form:"channel_id"`
+	ChannelName    string `form:"channel_name"`
+	UserID         string `form:"user_id"`
+	UserName       string `form:"user_name"`
+	Command        string `form:"command"`
+	Text           string `form:"text"`
+	ResponseURL    string `form:"response_url"`
 }
