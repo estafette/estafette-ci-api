e098a553d397cc2911f37c53aff7630bc04ffadd
Jorrit Salverda <jsalverda@travix.com>
2017-09-05 16:55:29 +0200

    use dollar placeholders for insert query

--- cockroach/cockroachDBClient.go
+++ cockroach/cockroachDBClient.go
@@ -114,11 +114,11 @@ func (dbc *cockroachDBClientImpl) InsertBuildJobLogs(buildJobLogs BuildJobLogs) 
 		)
 		VALUES
 		(
-			?,
-			?,
-			?,
-			?,
-			?
+			$1,
+			$2,
+			$3,
+			$4,
+			$5
 		)`,
 		buildJobLogs.RepoFullName,
 		buildJobLogs.RepoBranch,
