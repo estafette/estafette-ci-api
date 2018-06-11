0536d9c50d57b1e0ee74f8972a5c69f8f13fefe2
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-06-11 16:21:35 +0200

    updated contracts for json serialization

--- vendor/github.com/estafette/estafette-ci-contracts/build.go
+++ vendor/github.com/estafette/estafette-ci-contracts/build.go
@@ -4,16 +4,16 @@ import "time"
 
 // Build represents a specific build, including version number, repo, branch, revision, labels and manifest
 type Build struct {
-	ID           string    `jsonapi:"primary,builds"`
-	RepoSource   string    `jsonapi:"attr,repo-source"`
-	RepoOwner    string    `jsonapi:"attr,repo-owner"`
-	RepoName     string    `jsonapi:"attr,repo-name"`
-	RepoBranch   string    `jsonapi:"attr,repo-branch"`
-	RepoRevision string    `jsonapi:"attr,repo-revision"`
-	BuildVersion string    `jsonapi:"attr,build-version"`
-	BuildStatus  string    `jsonapi:"attr,build-status"`
-	Labels       string    `jsonapi:"attr,labels"`
-	Manifest     string    `jsonapi:"attr,manifest"`
-	InsertedAt   time.Time `jsonapi:"attr,inserted-at"`
-	UpdatedAt    time.Time `jsonapi:"attr,updated-at"`
+	ID           string    `jsonapi:"primary,builds" json:"id"`
+	RepoSource   string    `jsonapi:"attr,repo-source" json:"repoSource"`
+	RepoOwner    string    `jsonapi:"attr,repo-owner" json:"repoOwner"`
+	RepoName     string    `jsonapi:"attr,repo-name" json:"repoName"`
+	RepoBranch   string    `jsonapi:"attr,repo-branch" json:"repoBranch"`
+	RepoRevision string    `jsonapi:"attr,repo-revision" json:"repoRevision"`
+	BuildVersion string    `jsonapi:"attr,build-version" json:"buildVersion"`
+	BuildStatus  string    `jsonapi:"attr,build-status" json:"buildStatus"`
+	Labels       string    `jsonapi:"attr,labels" json:"labels"`
+	Manifest     string    `jsonapi:"attr,manifest" json:"manifest"`
+	InsertedAt   time.Time `jsonapi:"attr,inserted-at" json:"insertedAt"`
+	UpdatedAt    time.Time `jsonapi:"attr,updated-at" json:"updatedAt"`
 }
--- vendor/github.com/estafette/estafette-ci-contracts/pipeline.go
+++ vendor/github.com/estafette/estafette-ci-contracts/pipeline.go
@@ -4,16 +4,16 @@ import "time"
 
 // Pipeline represents a pipeline with the latest build info, including version number, repo, branch, revision, labels and manifest
 type Pipeline struct {
-	ID           string    `jsonapi:"primary,pipelines"`
-	RepoSource   string    `jsonapi:"attr,repo-source"`
-	RepoOwner    string    `jsonapi:"attr,repo-owner"`
-	RepoName     string    `jsonapi:"attr,repo-name"`
-	RepoBranch   string    `jsonapi:"attr,repo-branch"`
-	RepoRevision string    `jsonapi:"attr,repo-revision"`
-	BuildVersion string    `jsonapi:"attr,build-version"`
-	BuildStatus  string    `jsonapi:"attr,build-status"`
-	Labels       string    `jsonapi:"attr,labels"`
-	Manifest     string    `jsonapi:"attr,manifest"`
-	InsertedAt   time.Time `jsonapi:"attr,inserted-at"`
-	UpdatedAt    time.Time `jsonapi:"attr,updated-at"`
+	ID           string    `jsonapi:"primary,pipelines" json:"id"`
+	RepoSource   string    `jsonapi:"attr,repo-source" json:"repoSource"`
+	RepoOwner    string    `jsonapi:"attr,repo-owner" json:"repoOwner"`
+	RepoName     string    `jsonapi:"attr,repo-name" json:"repoName"`
+	RepoBranch   string    `jsonapi:"attr,repo-branch" json:"repoBranch"`
+	RepoRevision string    `jsonapi:"attr,repo-revision" json:"repoRevision"`
+	BuildVersion string    `jsonapi:"attr,build-version" json:"buildVersion"`
+	BuildStatus  string    `jsonapi:"attr,build-status" json:"buildStatus"`
+	Labels       string    `jsonapi:"attr,labels" json:"labels"`
+	Manifest     string    `jsonapi:"attr,manifest" json:"manifest"`
+	InsertedAt   time.Time `jsonapi:"attr,inserted-at" json:"insertedAt"`
+	UpdatedAt    time.Time `jsonapi:"attr,updated-at" json:"updatedAt"`
 }
