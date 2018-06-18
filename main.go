c9e37822c1eec9dc9220df6008e753917db95031
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-06-18 12:11:51 +0200

    restructure urls for integrations of bitbucket, github and slack

--- main.go
+++ main.go
@@ -268,12 +268,15 @@ func handleRequests(stopChannel <-chan struct{}, waitGroup *sync.WaitGroup) *htt
 
 	githubEventHandler := github.NewGithubEventHandler(githubPushEvents, *githubWebhookSecret, prometheusInboundEventTotals)
 	router.POST("/events/github", githubEventHandler.Handle)
+	router.POST("/api/integrations/github/events", githubEventHandler.Handle)
 
 	bitbucketEventHandler := bitbucket.NewBitbucketEventHandler(bitbucketPushEvents, prometheusInboundEventTotals)
 	router.POST("/events/bitbucket", bitbucketEventHandler.Handle)
+	router.POST("/api/integrations/bitbucket/events", bitbucketEventHandler.Handle)
 
 	slackEventHandler := slack.NewSlackEventHandler(secretHelper, *slackAppVerificationToken, slackEvents, prometheusInboundEventTotals)
 	router.POST("/events/slack/slash", slackEventHandler.Handle)
+	router.POST("/api/integrations/slack/slash", slackEventHandler.Handle)
 
 	estafetteEventHandler := estafette.NewEstafetteEventHandler(*estafetteCiAPIKey, estafetteCiBuilderEvents, estafetteBuildJobLogs, prometheusInboundEventTotals)
 	router.POST("/events/estafette/ci-builder", estafetteEventHandler.Handle)
