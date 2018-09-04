e0213c58673d3a6385a7a74eec5736ee22e31671
Jorrit Salverda <jsalverda@travix.com>
2018-09-04 11:18:27 +0200

    update zerolog to 1.8.0

--- govendor.sh
+++ govendor.sh
@@ -2,7 +2,7 @@ govendor init
 govendor fetch github.com/ericchiang/k8s/...@v1.1.0
 govendor fetch github.com/prometheus/client_golang/prometheus@v0.8.0
 govendor fetch github.com/prometheus/client_golang/prometheus/promhttp@v0.8.0
-govendor fetch github.com/rs/zerolog/log@v1.2.2
+govendor fetch github.com/rs/zerolog/...@v1.8.0
 govendor fetch github.com/alecthomas/kingpin@v2.2.5
 govendor fetch github.com/dgrijalva/jwt-go@v3.0.0
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
