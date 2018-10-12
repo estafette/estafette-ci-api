128dbb9e63391a097ccffeec7ac2b0c5f161135b
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-10-12 11:54:27 +0200

    fix route param that conflicts with existing wildcard

--- estafette/estafetteApiHandler.go
+++ estafette/estafetteApiHandler.go
@@ -756,7 +756,7 @@ func (h *apiHandlerImpl) GetPipelineReleaseLogs(c *gin.Context) {
 func (h *apiHandlerImpl) TailPipelineReleaseLogs(c *gin.Context) {
 	owner := c.Param("owner")
 	repo := c.Param("repo")
-	id := c.Param("id")
+	id := c.Param("revisionOrId")
 
 	jobName := h.ciBuilderClient.GetJobName("release", owner, repo, id)
 	h.ciBuilderClient.TailCiBuilderJobLogs(jobName)
