9d2832eeb93db708eafa70e7340a3c661ac2ee31
Jorrit Salverda <jsalverda@travix.com>
2018-12-07 17:40:55 +0100

    show warning when dev tag is used

--- estafette/warningHelper.go
+++ estafette/warningHelper.go
@@ -31,10 +31,14 @@ func (w *warningHelperImpl) GetManifestWarnings(manifest *manifest.EstafetteMani
 	if manifest != nil {
 		// check all build and release stages to have a pinned version
 		stagesUsingLatestTag := []string{}
+		stagesUsingDevTag := []string{}
 		for _, s := range manifest.Stages {
 			containerImageTag := getContainerImageTag(s.ContainerImage)
 			if containerImageTag == "latest" {
 				stagesUsingLatestTag = append(stagesUsingLatestTag, s.Name)
+			}
+			if containerImageTag == "dev" {
+				stagesUsingDevTag = append(stagesUsingDevTag, s.Name)
 			}
 		}
 
@@ -44,6 +48,9 @@ func (w *warningHelperImpl) GetManifestWarnings(manifest *manifest.EstafetteMani
 				if containerImageTag == "latest" {
 					stagesUsingLatestTag = append(stagesUsingLatestTag, fmt.Sprintf("%v/%v", r.Name, s.Name))
 				}
+				if containerImageTag == "dev" {
+					stagesUsingDevTag = append(stagesUsingDevTag, s.Name)
+				}
 			}
 		}
 
@@ -51,6 +58,12 @@ func (w *warningHelperImpl) GetManifestWarnings(manifest *manifest.EstafetteMani
 			warnings = append(warnings, contracts.Warning{
 				Status:  "warning",
 				Message: fmt.Sprintf("This pipeline has one or more stages that use **latest** or no tag for its container image: `%v`; it is [best practice](https://estafette.io/usage/best-practices/#pin-image-versions) to pin stage images to specific versions so you don't spend hours tracking down build failures because the used image has changed.", strings.Join(stagesUsingLatestTag, ", ")),
+			})
+		}
+		if len(stagesUsingDevTag) > 0 {
+			warnings = append(warnings, contracts.Warning{
+				Status:  "warning",
+				Message: fmt.Sprintf("This pipeline has one or more stages that use the **dev** tag for its container image: `%v`; it is [best practice](https://estafette.io/usage/best-practices/#avoid-using-estafette-s-dev-or-beta-tags) to avoid the dev tag alltogether, since it can be broken at any time.", strings.Join(stagesUsingDevTag, ", ")),
 			})
 		}
 	}
