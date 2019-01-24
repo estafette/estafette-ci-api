0a7960d13a818a3a2a197b37ff511bd97f3a2cd9
Jorrit Salverda <jsalverda@travix.com>
2019-01-24 15:14:18 +0100

    fix integration test

--- auth/helpers_test.go
+++ auth/helpers_test.go
@@ -11,19 +11,22 @@ import (
 func TestRetrievingIAPJSONWebKeys(t *testing.T) {
 	t.Run("ReturnsKeyByKeyID", func(t *testing.T) {
 
-		// act
-		publicKey, err := GetCachedIAPJsonWebKey("rTlk-g")
-
-		expectedX := new(big.Int)
-		expectedX, _ = expectedX.SetString("80705443177100294943977016489350978230449831162147377309904653463161053062518", 10)
-
-		expectedY := new(big.Int)
-		expectedY, _ = expectedY.SetString("39386914180697077709177261326057757164855031063540188737223679822281777517038", 10)
+		// act (if fails get new kid from https://www.gstatic.com/iap/verify/public_key-jwk and update expectancies until it works)
+		publicKey, err := GetCachedIAPJsonWebKey("FAWt5w")
 
 		if assert.Nil(t, err) {
 			assert.Equal(t, elliptic.P256(), publicKey.Curve)
-			assert.Equal(t, expectedX, publicKey.X)
-			assert.Equal(t, expectedY, publicKey.Y)
+
+			expectedX := new(big.Int)
+			expectedX, _ = expectedX.SetString("109310548864476541829996403431107199831106596242696403068756948144863290489665", 10)
+
+			if assert.Equal(t, expectedX, publicKey.X) {
+
+				expectedY := new(big.Int)
+				expectedY, _ = expectedY.SetString("15007658710064885800843054588135540073471183338568009446226392736133283689534", 10)
+
+				assert.Equal(t, expectedY, publicKey.Y)
+			}
 		}
 	})
 }
