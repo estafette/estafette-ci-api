0a6756b9c311ebe2fb433c76cd643c3ec26b7cfc
Jorrit Salverda <jsalverda@travix.com>
2019-03-04 09:08:16 +0100

    fix flaky integration test

--- auth/helpers_test.go
+++ auth/helpers_test.go
@@ -12,18 +12,18 @@ func TestRetrievingIAPJSONWebKeys(t *testing.T) {
 	t.Run("ReturnsKeyByKeyID", func(t *testing.T) {
 
 		// act (if fails get new kid from https://www.gstatic.com/iap/verify/public_key-jwk and update expectancies until it works)
-		publicKey, err := GetCachedIAPJsonWebKey("FAWt5w")
+		publicKey, err := GetCachedIAPJsonWebKey("f9R3yg")
 
 		if assert.Nil(t, err) {
 			assert.Equal(t, elliptic.P256(), publicKey.Curve)
 
 			expectedX := new(big.Int)
-			expectedX, _ = expectedX.SetString("109310548864476541829996403431107199831106596242696403068756948144863290489665", 10)
+			expectedX, _ = expectedX.SetString("33754992528993959342082873952071099444905807959681776349240807143574023195992", 10)
 
 			if assert.Equal(t, expectedX, publicKey.X) {
 
 				expectedY := new(big.Int)
-				expectedY, _ = expectedY.SetString("15007658710064885800843054588135540073471183338568009446226392736133283689534", 10)
+				expectedY, _ = expectedY.SetString("30017756976983295626595109856839943719662421701617989535808220756803905010317", 10)
 
 				assert.Equal(t, expectedY, publicKey.Y)
 			}
