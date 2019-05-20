31decccbd1ce6ecfde2d239877eb8a542f85411a
Jorrit Salverda <jsalverda@travix.com>
2019-05-20 17:36:46 +0200

    configure remote config endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -285,6 +286,18 @@ func initJaeger(service string) io.Closer {
 	cfg, err := jaegercfg.FromEnv()
 	if err != nil {
 		log.Fatal().Err(err).Msg("Generating Jaeger config from environment variables failed")
+	}
+
+	if os.Getenv("JAEGER_AGENT_HOST") != "" {
+		// get remote config from jaeger-agent running as daemonset
+		if cfg.Sampler.SamplingServerURL == "" {
+			cfg.Sampler.SamplingServerURL = fmt.Sprintf("http://%v:5778/sampling", os.Getenv("JAEGER_AGENT_HOST"))
+		}
+
+		// get remote config for baggage restrictions from jaeger-agent running as deamonset
+		if cfg.BaggageRestrictions.HostPort == "" {
+			cfg.BaggageRestrictions.HostPort = fmt.Sprintf("%v:5778", os.Getenv("JAEGER_AGENT_HOST"))
+		}
 	}
 
 	closer, err := cfg.InitGlobalTracer(service, jaegercfg.Logger(jaeger.StdLogger), jaegercfg.Metrics(jprom.New()))
