37725ebbb9bd7da1e46071030b353d1853942b99
Vitor Arins <vitorarins@gmail.com>
2020-03-31 16:21:14 +0200

    Add some tests for the cloudsource client.

---
+++ clients/cloudsourceapi/client_test.go
@@ -0,0 +1,76 @@
+package cloudsourceapi
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/estafette/estafette-ci-api/config"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	sourcerepo "google.golang.org/api/sourcerepo/v1"
+)
+
+func TestGetToken(t *testing.T) {
+
+	t.Run("ReturnsTokenForRepository", func(t *testing.T) {
+
+		ctx := context.Background()
+		tokenSource, err := google.DefaultTokenSource(ctx, sourcerepo.CloudPlatformScope)
+		assert.Nil(t, err)
+		sourcerepoService, err := sourcerepo.New(oauth2.NewClient(ctx, tokenSource))
+		assert.Nil(t, err)
+
+		cloudSourceConfig := config.CloudSourceConfig{
+			PrivateKeyPath: "",
+			WhitelistedOwners: []string{
+				"estafette",
+			},
+		}
+		client := NewClient(cloudSourceConfig, sourcerepoService, tokenSource)
+
+		// act
+		token, err := client.GetAccessToken(ctx)
+
+		expectedExpiry := time.Now().Local().Add(time.Hour * time.Duration(1))
+		assert.Nil(t, err)
+		assert.NotNil(t, token)
+		assert.Equal(t, expectedExpiry.Format("Jan 2 15:04"), token.Expiry.Format("Jan 2 15:04"))
+	})
+}
+
+func TestGetAuthenticatedRepositoryURL(t *testing.T) {
+
+	t.Run("ReturnsAuthenticatedURLForRepository", func(t *testing.T) {
+
+		ctx := context.Background()
+		tokenSource, err := google.DefaultTokenSource(ctx, sourcerepo.CloudPlatformScope)
+		assert.Nil(t, err)
+		sourcerepoService, err := sourcerepo.New(oauth2.NewClient(ctx, tokenSource))
+		assert.Nil(t, err)
+
+		cloudSourceConfig := config.CloudSourceConfig{
+			PrivateKeyPath: "",
+			WhitelistedOwners: []string{
+				"estafette",
+			},
+		}
+		client := NewClient(cloudSourceConfig, sourcerepoService, tokenSource)
+
+		// act
+		token, err := client.GetAccessToken(ctx)
+
+		expectedExpiry := time.Now().Local().Add(time.Hour * time.Duration(1))
+		assert.Nil(t, err)
+		assert.NotNil(t, token)
+		assert.Equal(t, expectedExpiry.Format("Jan 2 15:04"), token.Expiry.Format("Jan 2 15:04"))
+
+		url, err := client.GetAuthenticatedRepositoryURL(ctx, token, fmt.Sprintf("https://%v/p/%v/r/%v", "source.developers.google.com", "playground-varins-idpd0", "test-estafette-support"))
+		expectedUrl := "https://x-token-auth:.+@source\\.developers\\.google\\.com/p/playground-varins-idpd0/r/test-estafette-support"
+		assert.Nil(t, err)
+		assert.NotNil(t, url)
+		assert.Regexp(t, expectedUrl, url)
+	})
+}
