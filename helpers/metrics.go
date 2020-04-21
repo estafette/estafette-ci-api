f26d079ba384952114e2ffaf0e494391dbd450cf
Jorrit Salverda <jsalverda@travix.com>
2020-04-21 08:33:08 +0200

    allow multiple calls to NewRequestCounter and NewRequestHistogram; fixes #81

--- helpers/metrics.go
+++ helpers/metrics.go
@@ -16,20 +16,34 @@ func UpdateMetrics(requestCount metrics.Counter, requestLatency metrics.Histogra
 	requestLatency.With("func", funcName).Observe(time.Since(begin).Seconds())
 }
 
+var requestCounters map[string]metrics.Counter = map[string]metrics.Counter{}
+
 func NewRequestCounter(subsystem string) metrics.Counter {
-	return kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-		Namespace: "api",
-		Subsystem: subsystem,
-		Name:      "request_count",
-		Help:      "Number of requests received.",
-	}, []string{"func"})
+
+	if _, ok := requestCounters[subsystem]; !ok {
+		requestCounters[subsystem] = kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: subsystem,
+			Name:      "request_count",
+			Help:      "Number of requests received.",
+		}, []string{"func"})
+	}
+
+	return requestCounters[subsystem]
 }
 
+var requestHistograms map[string]metrics.Histogram = map[string]metrics.Histogram{}
+
 func NewRequestHistogram(subsystem string) metrics.Histogram {
-	return kitprometheus.NewHistogramFrom(stdprometheus.HistogramOpts{
-		Namespace: "api",
-		Subsystem: subsystem,
-		Name:      "request_latency_seconds",
-		Help:      "Total duration of requests in seconds.",
-	}, []string{"func"})
+
+	if _, ok := requestHistograms[subsystem]; !ok {
+		requestHistograms[subsystem] = kitprometheus.NewHistogramFrom(stdprometheus.HistogramOpts{
+			Namespace: "api",
+			Subsystem: subsystem,
+			Name:      "request_latency_seconds",
+			Help:      "Total duration of requests in seconds.",
+		}, []string{"func"})
+	}
+
+	return requestHistograms[subsystem]
 }
