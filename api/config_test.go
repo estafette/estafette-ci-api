9fd341b70d6749ac53938020f25cfad0a036f197
Jorrit Salverda <jsalverda@travix.com>
2020-08-14 15:27:38 +0200

    update manifest lib so injected stage name can be set

--- api/config_test.go
+++ api/config_test.go
@@ -155,6 +155,10 @@ func TestReadConfigFromFile(t *testing.T) {
 		assert.Equal(t, "database", apiServerConfig.LogWriters[0])
 		assert.Equal(t, "cloudstorage", apiServerConfig.LogWriters[1])
 		assert.Equal(t, "database", apiServerConfig.LogReader)
+		assert.Equal(t, "envvars", apiServerConfig.InjectStages.Build.Before[0].Name)
+		assert.Equal(t, "extensions/envvars:stable", apiServerConfig.InjectStages.Build.Before[0].ContainerImage)
+		assert.Equal(t, "envvars", apiServerConfig.InjectStages.Release.After[0].Name)
+		assert.Equal(t, "extensions/envvars:dev", apiServerConfig.InjectStages.Release.After[0].ContainerImage)
 	})
 
 	t.Run("ReturnsAuthConfig", func(t *testing.T) {
