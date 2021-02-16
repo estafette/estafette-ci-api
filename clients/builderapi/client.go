eb010dd229fdd493e173a69e359c6cdd50c3e20d
Jorrit Salverda <jsalverda@travix.com>
2021-02-16 10:10:21 +0100

    debug logging to see what triggered events look like once copied into builder config

--- clients/builderapi/client.go
+++ clients/builderapi/client.go
@@ -810,6 +810,8 @@ func (c *client) getBuilderConfig(ctx context.Context, ciBuilderParams CiBuilder
 		}
 	}
 
+	log.Debug().Interface("triggeredByEvents", ciBuilderParams.TriggeredByEvents).Interface("events", localBuilderConfig.Events).Msg("Showing builder config events")
+
 	return localBuilderConfig, nil
 }
 
