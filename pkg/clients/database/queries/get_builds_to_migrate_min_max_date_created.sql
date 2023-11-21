e073095941ec580cba0ea30fbbb5b75497be5588
Mo Alsharbaji <msharbaji@gmail.com>
2023-11-21 13:15:02 +0100

    fix the max get build date

--- pkg/clients/database/queries/get_builds_to_migrate_min_max_date_created.sql
+++ pkg/clients/database/queries/get_builds_to_migrate_min_max_date_created.sql
@@ -1,6 +1,6 @@
 SELECT
   COALESCE(MIN(inserted_at), '1970-01-01'::timestamptz) AS oldest_created_date,
-  COALESCE(MIN(inserted_at), '1970-01-01'::timestamptz)  AS max_created_date
+  COALESCE(Max(inserted_at), '1970-01-01'::timestamptz)  AS max_created_date
 FROM
   builds
 WHERE
